c4c79769e283eff62f26a3bad5a8d29fa321ed25
Robin Miller <rmillerpdx@gmail.com>
2017-10-05 11:26:48 -0700

    fix buffer variable in bitmap-transformer-test.js

--- test/bitmap-transformer.test.js
+++ test/bitmap-transformer.test.js
@@ -7,8 +7,9 @@ describe('bitmap file transformer', () => {
     
     let buffer = null;
     beforeEach(() => {
-        // TODO: read './test/test-bitmap.bmp' into buffer variable
+        // DONE TODO: read './test/test-bitmap.bmp' into buffer variable
         // Okay to use `sync` file methods for now
+        buffer = fs.readFileSync('./test/test-bitmap.bmp');
 
         // TODO: If the functionality in this before test is same as 
         // other test, can you remove (extract) the duplication?
@@ -29,8 +30,8 @@ describe('bitmap file transformer', () => {
 
         // read the output file we saved earlier as
         // the "standard" expected output file
-        const buffer = fs.readFileSync('./test/inverted-expected.bmp');
-        assert.deepEqual(bitmap.buffer, buffer);
+        const expected = fs.readFileSync('./test/inverted-expected.bmp');
+        assert.deepEqual(bitmap.buffer, expected);
 
         // if you don't have a standard file yet, you could write it 
         // out by commenting above code, using code below and visually inspect
